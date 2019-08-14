4c8f92a01e9499083815459be03dd08ff2b9ed51
Meo So <meowsoso@gmail.com>
2019-08-14 15:25:52 +1000

    update

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 // HashRouter is strongly preferred to save headaches with deployment.
-import { HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from "react-router-dom";
 
-import FlightSearch from './components/FlightSearch';
-import Flights from './components/Flights';
+import FlightSearch from "./components/FlightSearch";
+import Flights from "./components/Flights";
+import User from "./components/User";
 
 // Not a functional component. It's just a collection of JSX.
 const Routes = (
   <Router>
     <div>
-        <Route path="/Flights/:id" component={ Flights } />
-        <Route extend path="/FlightSearch" component={ FlightSearch} />
+      <Route path="/user" component={User} />
+      <Route path="/Flights/:id" component={Flights} />
+      <Route extend path="/FlightSearch" component={FlightSearch} />
     </div>
   </Router>
 );
